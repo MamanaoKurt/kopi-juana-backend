b82543368303cf534a2157045900a99d0310a474
Kurt Mamanao <midskun8@gmail.com>
2026-04-22 15:24:12 +0800

    Add missing visit facebook route

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GalleryController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
-
 Route::get('/', [GalleryController::class, 'index'])->name('home');
 Route::get('/gallery', [GalleryController::class, 'index'])->name('gallery');
 
-/* ADMIN */
 Route::get('/admin/gallery', [GalleryController::class, 'admin'])->name('admin.gallery');
 Route::post('/admin/gallery/upload', [GalleryController::class, 'upload'])->name('admin.gallery.upload');
 Route::delete('/admin/gallery/{galleryImage}', [GalleryController::class, 'destroy'])->name('admin.gallery.destroy');
+
+Route::get('/visit-facebook', function () {
+    return redirect()->away('https://www.facebook.com/share/18yqia5tWe/');
+})->name('visit.facebook');
